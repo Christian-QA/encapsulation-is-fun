bd11a139cea746649ff264033ed43b5ab793fedf
David <temiloluwaw@gmail.com>
2020-04-07 10:53:43 +0100

    update

--- src/com/qa/main/Animal.java
+++ src/com/qa/main/Animal.java
@@ -4,8 +4,26 @@ package com.qa.main;
 public class Animal {
 
 	// Fields
-	private int eyes = 12;
+	private int eyes = 16;
 	private String name;
+	private int legs = 15;
+	private String hello = "Yes";
+	
+	public int getLegs() {
+		return legs;
+	}
+
+	public void setLegs(int legs) {
+		this.legs = legs;
+	}
+
+	public String getHello() {
+		return hello;
+	}
+
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
 
 	public int getEyes() {
 		return eyes;
